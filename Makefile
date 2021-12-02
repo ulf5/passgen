1cb44a27194b25678cf772e13732a1107636a5a0
ulf <ulfahammar@gmail.com>
2021-12-02 19:19:13 +0100

    initial
    ...
    
    install command
    
    ...
    
    remove traces of dip

---
+++ Makefile
@@ -0,0 +1,16 @@
+.PHONY: dev-setup clean
+
+venv:
+	python -m venv venv
+
+dev-setup: venv
+	(. venv/bin/activate && pip install -e .)
+
+clean:
+	rm -rf venv
+	rm -rf build
+	find . -name "*.egg-info*" -exec rm -rv {} +
+	find . -name "*__pycache__*" -exec rm -rv {} +
+
+install:
+	pip install --user .
---
+++ src/passgen/__init__.py
@@ -0,0 +1,39 @@
+import secrets
+import string
+import sys
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+
+def cli():
+    parsed_args = _parse(sys.argv[1:])
+    print(generate_password(parsed_args.length, parsed_args.include_symbols))
+
+
+def generate_password(length, include_symbols=False):
+    alphabet = string.ascii_letters + string.digits
+    if include_symbols:
+        alphabet += string.punctuation
+
+    return ''.join(secrets.choice(alphabet) for i in range(length))
+
+
+def _parse(args):
+    parser = ArgumentParser(
+        description='passgen',
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        'length',
+        type=int,
+        nargs=None
+    )
+    parser.add_argument(
+        '-s',
+        '--include-symbols',
+        help='include symbols',
+        default=False,
+        action='store_true'
+    )
+    return parser.parse_intermixed_args(args)
